3a7a547d61555a8cb07a879f1adf15ac6042a4a2
Annamalai Palanikumar <annamalai@outlook.in>
2023-02-05 19:01:07 +0100

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -16,35 +16,35 @@ do
   echo -n "Installing $pkg ..."
   case $pkg in
     jre)
-      sudo apt-get install default-jre
+      sudo apt-get -y install default-jre
       ;;
     jdk)
-      sudo apt-get install default-jdk
+      sudo apt-get -y install default-jdk
       ;;
     mysql)
-      sudo apt-get install mysql-server
+      sudo apt-get -y install mysql-server
       ;;
     jenkins)
       curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
       echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-      sudo apt-get install jenkins
+      sudo apt-get -y install jenkins
       ;;
     nginx)
-      sudo apt-get install certbot python3-certbot-nginx
-      sudo apt-get install nginx
+      sudo apt-get -y install certbot python3-certbot-nginx
+      sudo apt-get -y install nginx
       sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 80 -j ACCEPT && sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 443 -j ACCEPT && sudo netfilter-persistent save
       ;;
     git)
-      sudo apt-get install git
+      sudo apt-get -y install git
       ;;
     maven)
-      sudo apt-get install maven
+      sudo apt-get -y install maven
       ;;
     vscode)
       wget -O- https://aka.ms/install-vscode-server/setup.sh | sh
       ;;
     *)
-      sudo apt-get install $pkg
+      sudo apt-get -y install $pkg
       ;;
   esac
 done
