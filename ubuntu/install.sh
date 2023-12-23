806d95087d3eb744695090cee9906a7ba2ffd8ae
Annamalai Palanikumar <annamalai@outlook.in>
2023-12-23 21:49:46 +0100

    Jenkins Installation failure fixes.

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -30,7 +30,7 @@ do
       sudo systemctl restart mysql
       ;;
     jenkins)
-      curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+      curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
       echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
       sudo apt-get update
       sudo apt-get -y install jenkins
